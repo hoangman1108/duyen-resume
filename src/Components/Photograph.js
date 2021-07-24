b719a7988c312795cf16651160777a332bfcc2d8
hoangman1108 <hoangman772@gmail.com>
2021-07-24 20:18:57 +0700

    update photo

--- src/Components/Photograph.js
+++ src/Components/Photograph.js
@@ -1,43 +1,76 @@
 import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
+const images = [
+  'images/photo/photo_05.jpg',
+  'images/photo/photo_06.jpg',
+  'images/photo/photo_03.jpg',
+  'images/photo/photo_04.jpg',
+  'images/photo/photo_02.jpg',
+  'images/photo/photo_01.jpg',
+];
 class PhotoGraph extends Component {
-  render() {
+  constructor(props) {
+    super(props);
 
-    if(this.props.data){
-      var projects = this.props.data.photo.map(function(projects){
-        var projectImage = 'images/photo/'+projects.image;
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const { photoIndex, isOpen } = this.state;
+    const changeState = () =>{
+      console.log('go here')
+      this.setState({ isOpen: true })
+    }
+    if (this.props.data) {
+      var projects = this.props.data.photo.map(function (projects) {
+        var projectImage = 'images/photo/' + projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} target="_blank" title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               {/* <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div> */}
-              {/* <div className="link-icon"><i className="fa fa-link"></i></div> */}
-            </a>
+          <div className="item-wrap">
+            <img alt={projects.title} src={projectImage} onClick={changeState} />
           </div>
         </div>
       })
     }
 
     return (
-      <section id="portfolio" style={{paddingTop: "10px", background: 'none'}}>
+      <section id="portfolio" style={{ paddingTop: "10px", background: 'none' }}>
 
-      <div className="row">
+        <div className="row">
 
-         <div className="twelve columns collapsed">
+          <div className="twelve columns collapsed">
 
             <h1>Check Out Some of My Film Photos.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+              {projects}
+
+              {isOpen && (
+                <Lightbox
+                  mainSrc={images[photoIndex]}
+                  nextSrc={images[(photoIndex + 1) % images.length]}
+                  prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                  onCloseRequest={() => this.setState({ isOpen: false })}
+                  onMovePrevRequest={() =>
+                    this.setState({
+                      photoIndex: (photoIndex + images.length - 1) % images.length,
+                    })
+                  }
+                  onMoveNextRequest={() =>
+                    this.setState({
+                      photoIndex: (photoIndex + 1) % images.length,
+                    })
+                  }
+                />
+              )}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
